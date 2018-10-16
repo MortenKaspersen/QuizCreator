b0255a07c26d517320fa3baf5d085b424077bf9c
MortenKaspersen <mortenps@hotmail.com>
2018-10-16 19:58:13 +0200

    Add project files.

---
+++ QuizCreator/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(QuizCreator.Startup))]
+namespace QuizCreator
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
