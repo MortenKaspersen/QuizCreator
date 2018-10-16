f6a55e114420e6bec064f5ee43453b823985feb8
MortenKaspersen <mortenps@hotmail.com>
2018-10-16 20:10:55 +0200

    Added Question to Models

---
+++ QuizCreator/Models/Question.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QuizCreator.Models
+{
+    public class Question
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string QuestionText { get; set; }
+
+        public string AnswerOne { get; set; }
+
+        public string AnswerTwo { get; set; }
+
+        public string AnswerThree { get; set; }
+
+        public string ExplanationText { get; set; }
+        
+        public DateTime CreatedAt { get; set; }
+
+        
+    }
+}
